e27665ff80a1f110e5964035125eb6966f31c6d5
pisineen <49302144+pisineen@users.noreply.github.com>
2021-12-03 15:45:22 +0700

    Update check-quality-gate.sh

--- script/check-quality-gate.sh
+++ script/check-quality-gate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #source "$(dirname "$0")/common.sh"
-source "./common.sh"
+source "$(dirname "$0")/common.sh"
 
 if [[ -z "${SONAR_TOKEN}" ]]; then
   echo "Set the SONAR_TOKEN env variable."
